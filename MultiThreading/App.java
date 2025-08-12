0f7af9f751b13b3b31231eec6e276125e651a41d
Sahil161967 <sahilbajkamble@gmail.com>
2025-08-12 20:18:00 +0530

    Single thread example

---
+++ MultiThreading/App.java
@@ -0,0 +1,23 @@
+package com.tap.sahil.demo;
+
+
+public class App 
+{
+    static void serveCustomer(String name){
+        System.out.println(name+" service started ...");
+        try {
+            Thread.sleep(2000);
+            
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name+" service finished !!!");
+
+    }
+    public static void main( String[] args )
+    {
+       serveCustomer("Customer1");
+       serveCustomer("Customer2");
+       serveCustomer("Customer3");
+    }
+}
