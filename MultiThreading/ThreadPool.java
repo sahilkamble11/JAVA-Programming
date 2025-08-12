18e1099e01501396e0b11099e7b8279e3ecbc698
Sahil161967 <sahilbajkamble@gmail.com>
2025-08-12 20:21:35 +0530

    ThreadPool concept

---
+++ MultiThreading/ThreadPool.java
@@ -0,0 +1,26 @@
+package com.tap.sahil.demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPool {
+
+    static void serveCustomer(String name){
+        System.out.println(name+" served by "+Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        ExecutorService service=Executors.newFixedThreadPool(4);
+        for(int i=1;i<=5;i++){
+            final String customer="customer "+i;
+            service.submit(() -> serveCustomer(customer));
+
+        }
+        service.shutdown();
+    }
+    
+}
